b7076071084dbb2e661c71c2e233fba0464236cb
Феликс Хафизов <f_khafizov@protonmail.com>
2022-08-04 13:19:39 +0300

    Add Ukraine stripe

---
+++ components/help-ukraine.tsx
@@ -0,0 +1,17 @@
+export const HelpUkraine = () => (
+  <div sx={{ textAlign: 'center', p: 5 }}>
+    <p>
+      <a
+        sx={{
+          boxShadow: '0 4px 0 #005cbc, 0 8px 0 #ffcd00',
+          textDecoration: 'none',
+        }}
+        href="https://u24.gov.ua"
+        target="_blank"
+        rel="noopener noreferrer"
+      >
+        Help Ukraine Win
+      </a>
+    </p>
+  </div>
+)
--- pages/_app.tsx
+++ pages/_app.tsx
@@ -6,11 +6,13 @@ import Header from 'components/header'
 import { theme } from '@/styles/theme'
 import Layout from 'components/layout'
 import { NextIntlProvider } from 'next-intl'
+import { HelpUkraine } from 'components/help-ukraine'
 
 export default function MyApp({ Component, pageProps }: AppProps) {
   return (
     <NextIntlProvider messages={pageProps.messages}>
       <ThemeProvider theme={theme}>
+        <HelpUkraine />
         <Header />
         <Layout>
           <Component {...pageProps} />
